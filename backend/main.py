c7554d860146e35c13bb15cca10001ecc10708b7
Jayyadav1903 <rachana7398@gmail.com>
2026-04-03 20:13:48 +0530

    Massive improvements done such as using redis,celery and docker containerization

--- backend/main.py
+++ backend/main.py
@@ -31,7 +31,7 @@ app = FastAPI()
 # --- RATE LIMITING ---
 REDIS_URL = os.getenv("REDIS_URL")
 
-limiter = Limiter(key_func=get_remote_address, storage_url=REDIS_URL)
+limiter = Limiter(key_func=get_remote_address, storage_uri=REDIS_URL)
 app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded,_rate_limit_exceeded_handler)
 
